830d39e1f6beb1880823f4c20770850acae2e6da
Julio Silva <juliosns@gmail.com>
2022-12-30 03:51:30 +0100

    removing js from invite btn

--- js/default.js
+++ js/default.js
@@ -1,11 +1,4 @@
 window.addEventListener("load", (event) => {
-    document.querySelector('#confirm-btn').addEventListener('click', () => {
-        document.querySelector('.form').style.display = 'block';
-        document.querySelector('.map').style.display = 'none';
-        document.querySelector('.modal').style.display = 'block';
-        document.querySelector('.modal').style.opacity = 1;
-    });
-
     document.querySelector('#date-btn').addEventListener('click', () => {
         document.querySelector('.form').style.display = 'none';
         document.querySelector('.map').style.display = 'block';
